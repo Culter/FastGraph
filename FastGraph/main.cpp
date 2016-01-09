830315160687b233d0146605b0d4df821bcbbc03
culter <culter@culter-macbookpro.local>
2016-01-09 00:28:15 -0800

    Initial Commit

---
+++ FastGraph/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FastGraph
+//
+//  Created by culter on 1/9/16.
+//  Copyright © 2016 culter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
